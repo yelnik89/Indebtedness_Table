040cfb04e14189d369b165ed5e16a1a9e52c7862
yelnik89 <denis.yelnik@icloud.com>
2020-10-23 11:40:50 +0700

    add Indebtedness model

---
+++ Test_Task/Indebtedness.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_Task
+{
+    class Indebtedness
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SomethingOne { get; set; }
+        public bool SomethingTwo { get; set; }
+        public int SinAccount { get; set; }
+        public int KOSGU { get; set; }
+        public float StartYearAll { get; set; }
+        public float StartYearLong { get; set; }
+        public float StartYearOverdue { get; set; }
+        public float IncreaseIndebtednessAll { get; set; }
+        public float IncreaseIndebtednessNotMoney { get; set; }
+        public float DecreaseIndebtednessAll { get; set; }
+        public float DecreaseIndebtednessNotMoney { get; set; }
+        public float EndYearAll { get; set; }
+        public float EndYearLong { get; set; }
+        public float EndYearOverdue { get; set; }
+        public float EndYearAllPast { get; set; }
+        public float EndYearLongPast { get; set; }
+        public float EndYearOverduePast { get; set; }
+    }
+}
