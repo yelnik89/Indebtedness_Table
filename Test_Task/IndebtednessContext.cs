099d13beabd5c9dad7167cfbde3b8ffe370ad248
yelnik89 <denis.yelnik@icloud.com>
2020-10-23 11:44:54 +0700

    add context

---
+++ Test_Task/IndebtednessContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace Test_Task
+{
+    class IndebtednessContext : DbContext
+    {
+        public IndebtednessContext() : base(nameOrConnectionString: "DbConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Indebtedness> Indebtednesses { get; set; }
+    }
+}
